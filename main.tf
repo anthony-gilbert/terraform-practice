1dc9bc1dcd87abb0e7cbc7a20300c143df9e8cf8
Anthony Gilbert <anthony.gilbert@effectual.com>
2022-12-05 16:15:37 -0600

    Created a new flag called 'Name' to name the EC2 instance

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ provider "aws" {
 resource "aws_instance" "instance_1" {
   ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type   = "t2.micro"
+  
+  tags = {
+    Name = "ubuntu-20.04-instance"
+  }
 }
